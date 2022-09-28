d6eee263dab88ec56f95c911fafdd72782b139fd
schmam <35980147+schmam@users.noreply.github.com>
2022-09-28 16:42:38 -0500

    Update LootQuest.py

--- LootQuest.py
+++ LootQuest.py
@@ -1,28 +1,28 @@
 import random           # for all things RNG
 
 class Monster:
-    def __init__(self, type, health):
-        self.type = type
-        self.health = health
-        self.monster_types = [rat, goblin, orc]
-
+    def __init__(self, monster_type, monster_health):
+        self.monster_type = monster_type
+        self.monster_health = monster_health
+        
     @classmethod    
     def generate_monster(cls):
-        type = random.choice(monster_types)
-        if type == "rat":
-            health = random.randint(50, 100)
-        elif type == "goblin":
-            health = random.randint(100, 200)
-        elif type == "orc":
-            health = random.randint(200, 400)
-        return cls(type, health) 
+        monster_types = ["rat", "goblin", "orc"]
+        monster_type = random.choice(monster_types)
+        if monster_type == "rat":
+            monster_health = random.randint(50, 100)
+        elif monster_type == "goblin":
+            monster_health = random.randint(100, 200)
+        elif monster_type == "orc":
+            monster_health = random.randint(200, 400)
+        return cls(monster_type, monster_health) 
 
 
 
 def main():
     first_monster = Monster.generate_monster()
-    print(first_monster.type)
-    print(first_monster.health)
+    print(first_monster.monster_type)
+    print(first_monster.monster_health)
 
    
 
